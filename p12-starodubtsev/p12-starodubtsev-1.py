178feb4856c01f20991f885e1b12f86494acd06d
managercoller <91118200+managercoller@users.noreply.github.com>
2021-11-25 16:29:13 +0200

    dd

---
+++ p12-starodubtsev/p12-starodubtsev-1.py
@@ -0,0 +1,55 @@
+dirs = [
+    ( 'folder1',
+        [
+            'file1',
+            ( 'folder2', 
+                [
+                    'file2',
+                    'file3'
+                ] 
+            ),
+            ( 'folder3', 
+                [
+                    'file3', 
+                    'file4',
+                    ('folder4', ['file3'])
+                ] 
+            ),
+            'file5'
+        ]
+    )
+]
+
+# ВАШ КОД ТУТ
+from functools import reduce
+def search(dirs, filename, folder = ''):
+    result = []
+    if isinstance(dirs,list) or isinstance(dirs,tuple):
+        if isinstance(dirs,tuple):
+            if len(folder) == 0:
+                folder = folder + '/' + dirs[0]
+            else:
+                folder = folder + '//' + dirs[0]
+            dirs = dirs[1]
+        result = result + reduce(lambda result,node: result + search(node,filename,folder),dirs,[])
+    elif dirs == filename:
+        result.append(folder + '/' + filename)
+    return result
+# ПЕРЕВІРКА
+
+print(search(dirs, 'file1'))
+print(search(dirs, 'file2'))
+print(search(dirs, 'file3'))
+print(search(dirs, 'file4'))
+print(search(dirs, 'file5'))
+print(search(dirs, 'file6'))
+print(search(dirs, 'folder1'))
+
+assert search(dirs, 'file1') == ['/folder1/file1'], 'Failed test for file1'
+assert search(dirs, 'file2') == ['/folder1//folder2/file2'], 'Failed test for file2'
+assert search(dirs, 'file3') == ['/folder1//folder2/file3', '/folder1//folder3/file3', '/folder1//folder3//folder4/file3'], 'Failed test for file3'
+assert search(dirs, 'file4') == ['/folder1//folder3/file4'], 'Failed test for file4'
+assert search(dirs, 'file5') == ['/folder1/file5'], 'Failed test for file5'
+assert search(dirs, 'file6') == [], 'Failed test for file6'
+assert search(dirs, 'folder1') == [], 'Failed test for folder1'
+print('All tests good!')
